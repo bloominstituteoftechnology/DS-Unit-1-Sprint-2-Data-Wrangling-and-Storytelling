d50ded368e55b39454f24ef46a427cb7c24895e3
larkeith <llarkeith@gmail.com>
2019-09-11 09:23:21 -0700

    Import assignment as vscode jupyter

---
+++ module3-make-explanatory-visualizations/LS_DS_123_Make_Explanatory_Visualizations_Assignment.py
@@ -0,0 +1,67 @@
+#%% Change working directory from the workspace root to the ipynb file location. Turn this addition off with the DataScience.changeDirOnImportExport setting
+import os
+try:
+	os.chdir(os.path.join(os.getcwd(), 'module3-make-explanatory-visualizations'))
+	print(os.getcwd())
+except:
+	pass
+#%% [markdown]
+# <a href="https://colab.research.google.com/github/ryanleeallred/DS-Unit-1-Sprint-2-Data-Wrangling-and-Storytelling/blob/master/module3-make-explanatory-visualizations/LS_DS_123_Make_Explanatory_Visualizations_Assignment.ipynb" target="_parent"><img src="https://colab.research.google.com/assets/colab-badge.svg" alt="Open In Colab"/></a>
+#%% [markdown]
+# # ASSIGNMENT
+# 
+# ### 1) Replicate the lesson code. I recommend that you [do not copy-paste](https://docs.google.com/document/d/1ubOw9B3Hfip27hF2ZFnW3a3z9xAgrUDRReOEo-FHCVs/edit).
+# 
+# Get caught up to where we got our example in class and then try and take things further. How close to "pixel perfect" can you make the lecture graph?
+# 
+# Once you have something that you're proud of, share your graph in the cohort channel and move on to the second exercise.
+# 
+# ### 2) Reproduce another example from [FiveThityEight's shared data repository](https://data.fivethirtyeight.com/).
+# 
+# **WARNING**: There are a lot of very custom graphs and tables at the above link. I **highly** recommend not trying to reproduce any that look like a table of values or something really different from the graph types that we are already familiar with. Search through the posts until you find a graph type that you are more or less familiar with: histogram, bar chart, stacked bar chart, line chart, [seaborn relplot](https://seaborn.pydata.org/generated/seaborn.relplot.html), etc. Recreating some of the graphics that 538 uses would be a lot easier in Adobe photoshop/illustrator than with matplotlib. 
+# 
+# - If you put in some time to find a graph that looks "easy" to replicate you'll probably find that it's not as easy as you thought. 
+# 
+# - If you start with a graph that looks hard to replicate you'll probably run up against a brick wall and be disappointed with your afternoon.
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+#%%
+# Your Work Here
+
+#%% [markdown]
+# # STRETCH OPTIONS
+# 
+# ### 1) Reproduce one of the following using the matplotlib or seaborn libraries:
+# 
+# - [thanksgiving-2015](https://fivethirtyeight.com/features/heres-what-your-part-of-america-eats-on-thanksgiving/) 
+# - [candy-power-ranking](https://fivethirtyeight.com/features/the-ultimate-halloween-candy-power-ranking/) 
+# - or another example of your choice!
+# 
+# ### 2) Make more charts!
+# 
+# Choose a chart you want to make, from [Visual Vocabulary - Vega Edition](http://ft.com/vocabulary).
+# 
+# Find the chart in an example gallery of a Python data visualization library:
+# - [Seaborn](http://seaborn.pydata.org/examples/index.html)
+# - [Altair](https://altair-viz.github.io/gallery/index.html)
+# - [Matplotlib](https://matplotlib.org/gallery.html)
+# - [Pandas](https://pandas.pydata.org/pandas-docs/stable/visualization.html)
+# 
+# Reproduce the chart. [Optionally, try the "Ben Franklin Method."](https://docs.google.com/document/d/1ubOw9B3Hfip27hF2ZFnW3a3z9xAgrUDRReOEo-FHCVs/edit) If you want, experiment and make changes.
+# 
+# Take notes. Consider sharing your work with your cohort!
+
+#%%
+# More Work Here
+
+
